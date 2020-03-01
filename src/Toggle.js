e8b6a1a8280e3b72219833924b3e9c49434deefa
millord <juancarlosmillord@gmail.com>
2020-03-01 14:20:33 -0400

    added google maps to the package

--- src/Toggle.js
+++ src/Toggle.js
@@ -21,8 +21,8 @@ export const Toggle = () => {
           Desarrolla tus habilidades utilizando <strong> Qgis</strong> para la
           parte de escritorio de análisis geospacial. Llega hasta el próximo
           nivel haciendo uso de{" "}
-          <strong>React.js, Redux, Turf.js, Leaflet.js</strong> para crear mapas
-          interactivos.
+          <strong>React.js, Redux, Turf.js, Leaflet.js, Google Maps</strong>{" "}
+          para crear mapas interactivos.
         </animated.p>
       </div>
     </div>
